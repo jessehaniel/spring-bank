d19181e6bb7fbf4b6c657924f8a9ae59d834aa17
jessehaniel <jesse.haniel@gmail.com>
2020-11-10 21:12:02 -0300

    Removing legacy and unused code. Also, fixing h2 access control.

--- src/main/java/com/letscode/java/springbank/config/SecurityConfig.java
+++ src/main/java/com/letscode/java/springbank/config/SecurityConfig.java
@@ -1,39 +1,26 @@
 package com.letscode.java.springbank.config;
 
 import com.letscode.java.springbank.security.LetsCodePasswordEncoderFactories;
-import com.letscode.java.springbank.security.RestHeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Profile("custom-security")
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
-    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
-        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
-    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-            UsernamePasswordAuthenticationFilter.class);
-        
         http
             .csrf().disable()
             .authorizeRequests(authorize -> {
                 authorize
+                    .antMatchers("/h2-console/**").permitAll()
                     .mvcMatchers("/users").permitAll();
 //                  .antMatchers("/**").permitAll()
             })
@@ -42,6 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin().and()
             .httpBasic();
+    
+        //h2 console config
+        http.headers().frameOptions().sameOrigin();
     }
     
     @Bean
@@ -49,35 +39,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return LetsCodePasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
     
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("letscode")
-            .password("{bcrypt}$2a$10$HMiL2rbcF1HjFSMi4mydAu4Uw2T1LxuemoJk9gu.jEWWWsV/oHram")//admin123
-            .roles("ADMIN")
-            .and()
-            .withUser("professor")//password
-            .password("{sha256}5dc411bd616087d9427282f95abe9093529299cb179302becd31d59bacc8ca9585c1054ce8cf959c")
-            .roles("PROFESSOR")
-            .and()
-            .withUser("aluno")
-            .password("{ldap}{SSHA}ub2AAm+7NnZk+b657zQUn59OTy4GZdU6CgkpbA==")//aluno
-            .roles("ALUNO")
-            .and()
-            .withUser("guest")
-            .password("{noop}guest123")
-            .roles("GUEST");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//            .withUser("letscode")
+//            .password("{bcrypt}$2a$10$HMiL2rbcF1HjFSMi4mydAu4Uw2T1LxuemoJk9gu.jEWWWsV/oHram")//admin123
+//            .roles("ADMIN")
+//            .and()
+//            .withUser("professor")//password
+//            .password("{sha256}5dc411bd616087d9427282f95abe9093529299cb179302becd31d59bacc8ca9585c1054ce8cf959c")
+//            .roles("PROFESSOR")
+//            .and()
+//            .withUser("aluno")
+//            .password("{ldap}{SSHA}ub2AAm+7NnZk+b657zQUn59OTy4GZdU6CgkpbA==")//aluno
+//            .roles("ALUNO")
+//            .and()
+//            .withUser("guest")
+//            .password("{noop}guest123")
+//            .roles("GUEST");
+//    }
     
     //    @Override
 //    @Bean
 //    protected UserDetailsService userDetailsService() {
-//        final UserDetails admin = User.withDefaultPasswordEncoder()
+//        final UserDetails admin = UserBean.withDefaultPasswordEncoder()
 //            .username("admin")
 //            .password("admin123")
 //            .roles("ADMIN")
 //            .build();
-//        final UserDetails guest = User.withDefaultPasswordEncoder()
+//        final UserDetails guest = UserBean.withDefaultPasswordEncoder()
 //            .username("guest")
 //            .password("guest123")
 //            .roles("GUEST")
